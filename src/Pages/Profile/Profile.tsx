67be0652751660577e89f260534437511bec780d
Piyush Kokane <piyush.kokane.in@gmail.com>
2025-03-09 18:21:59 +0530

    User Data storage more improved - 2

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,7 +1,61 @@
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { UserData  } from "../../Context/UserDataContext";
 import "./Profile.css";
 import Footer from "../../Components/Footer/Footer";
+
+
+
+// type def for UserData
+interface UserDataType {
+    Key: string;
+    value: string;
+}
+
+
+// Sample UserData
+const sampleUserDocuments = [
+    { Key: "10th Marksheet", value: "/User_Data/10th_Marlsheet.png" },
+    { Key: "12th Marksheet", value: "/User_Data/12th_Marlsheet.png" },
+    { Key: "Leaving Certificate", value: "/User_Data/Leaving_Certificate.png" },
+    { Key: "Signature", value: "/User_Data/Signature.png" },
+];
+
+const sampleStudentInfo = [
+    { Key: "Gender", value: "Male" },
+    { Key: "Date Of Birth", value: "18/2/2005" },
+    { Key: "Blood Group", value: "AB+" },
+    { Key: "gmail", value: "google@gmail.com" },
+    { Key: "Phone No.", value: "8806808503" },
+    { Key: "Emergency No.", value: "8806808503" },
+    { Key: "Aadhar No.", value: "8806808503" },
+    { Key: "Admission Type", value: "CET" },
+    { Key: "Admission Status", value: "Active" },
+    { Key: "Admission Date", value: "24/3/2023" },
+];
+
+const sampleStudentAddress = [
+    { Key: "Permanent Address", value: "Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address " },
+    { Key: "Current Address", value: "Address" },
+    { Key: "Area", value: "Area" },
+    { Key: "Land Mark", value: "Land Mark" },
+    { Key: "City", value: "Pune" },
+    { Key: "Pincode", value: "411038" },
+    { Key: "State", value: "Maharashtra" },
+    { Key: "Country", value: "India" },
+];
+
+const sampleParent1Info = [
+    { Key: "Guardian Type", value: "Father" },
+    { Key: "Fathers's gmail", value: "google@gmail.com" },
+    { Key: "Father's Contact", value: "9875642587" },
+    { Key: "Father's Profession", value: "Job" },
+];
+const sampleParent2Info = [
+    { Key: "Guardian Type", value: "Mother" },
+    { Key: "Mother's gmail", value: "google@gmail.com" },
+    { Key: "Mother's Contact", value: "9875642587" },
+    { Key: "Mother's Profession", value: "Housewife" },
+];
 
 
 
@@ -16,50 +70,6 @@ function Profile() {
     let LongBio = user?.LongBio;
     let Biotag = user?.Biotag;
 
-    const UserDocuments = [
-        { Key: "10th Marksheet", img: "/User_Data/10th_Marlsheet.png" },
-        { Key: "12th Marksheet", img: "/User_Data/12th_Marlsheet.png" },
-        { Key: "Leaving Certificate", img: "/User_Data/Leaving_Certificate.png" },
-        { Key: "Signature", img: "/User_Data/Signature.png" },
-    ];
-
-    const StudentInfo = [
-        { Key: "Gender", value: "Male" },
-        { Key: "Date Of Birth", value: "18/2/2005" },
-        { Key: "Blood Group", value: "AB+" },
-        { Key: "gmail", value: "google@gmail.com" },
-        { Key: "Phone No.", value: "8806808503" },
-        { Key: "Emergency No.", value: "8806808503" },
-        { Key: "Aadhar No.", value: "8806808503" },
-        { Key: "Admission Type", value: "CET" },
-        { Key: "Admission Status", value: "Active" },
-        { Key: "Admission Date", value: "24/3/2023" },
-    ];
-
-    const StudentAddress = [
-        { Key: "Permanent Address", value: "Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address Address " },
-        { Key: "Current Address", value: "Address" },
-        { Key: "Area", value: "Area" },
-        { Key: "Land Mark", value: "Land Mark" },
-        { Key: "City", value: "Pune" },
-        { Key: "Pincode", value: "411038" },
-        { Key: "State", value: "Maharashtra" },
-        { Key: "Country", value: "India" },
-    ];
-
-    const Parent1Info = [
-        { Key: "Guardian Type", value: "Father" },
-        { Key: "Fathers's gmail", value: "google@gmail.com" },
-        { Key: "Father's Contact", value: "9875642587" },
-        { Key: "Father's Profession", value: "Job" },
-    ];
-    const Parent2Info = [
-        { Key: "Guardian Type", value: "Mother" },
-        { Key: "Mother's gmail", value: "google@gmail.com" },
-        { Key: "Mother's Contact", value: "9875642587" },
-        { Key: "Mother's Profession", value: "Housewife" },
-    ];
-
     const ProgramInfo = [
         { Key: "PRN", value: user?.Prn },
         { Key: "Branch", value: user?.Branch },
@@ -67,7 +77,49 @@ function Profile() {
     ];
 
 
+    // get data from localStorage, if not set fetch from api
+    const [UserDocuments] = useState(
+        JSON.parse(localStorage.getItem("UserDocuments") || JSON.stringify(sampleUserDocuments))
+    );
+    
+    const [StudentInfo] = useState(
+        JSON.parse(localStorage.getItem("StudentInfo") || JSON.stringify(sampleStudentInfo))
+    );
 
+    const [StudentAddress] = useState(
+        JSON.parse(localStorage.getItem("StudentAddress") || JSON.stringify(sampleStudentAddress))
+    );
+
+    const [Parent1Info] = useState(
+        JSON.parse(localStorage.getItem("Parent1Info") || JSON.stringify(sampleParent1Info))
+    );
+
+    const [Parent2Info] = useState(
+        JSON.parse(localStorage.getItem("Parent2Info") || JSON.stringify(sampleParent2Info))
+    );
+
+
+    // Update localStorage if not set
+    useEffect(() => {
+      if (!localStorage.getItem("UserDocuments"))
+        localStorage.setItem("UserDocuments", JSON.stringify(UserDocuments));
+
+      if (!localStorage.getItem("StudentInfo"))
+        localStorage.setItem("StudentInfo", JSON.stringify(StudentInfo));
+    
+      if (!localStorage.getItem("StudentAddress"))
+        localStorage.setItem("StudentAddress", JSON.stringify(StudentAddress));
+    
+      if (!localStorage.getItem("Parent1Info"))
+        localStorage.setItem("Parent1Info", JSON.stringify(Parent1Info));
+    
+      if (!localStorage.getItem("Parent2Info"))
+        localStorage.setItem("Parent2Info", JSON.stringify(Parent2Info));
+      
+    }, []);
+
+
+    
     function About() {    
         return (
             <>
@@ -92,7 +144,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>More Information</h1>
                     <div className="page-container-line-1"/>
-                    {StudentInfo.map((item) => (
+                    {StudentInfo.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -106,7 +158,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>Student Address</h1>
                     <div className="page-container-line-1"/>
-                    {StudentAddress.map((item) => (
+                    {StudentAddress.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -120,7 +172,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>Parent/Guardian Information</h1>
                     <div className="page-container-line-1"/>
-                    {Parent1Info.map((item) => (
+                    {Parent1Info.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -129,7 +181,7 @@ function Profile() {
                     ))}
     
                     <div className="page-container-line-2"/>
-                    {Parent2Info.map((item) => (
+                    {Parent2Info.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -165,7 +217,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>More Information</h1>
                     <div className="page-container-line-1"/>
-                    {StudentInfo.map((item) => (
+                    {StudentInfo.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -179,7 +231,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>Student Address</h1>
                     <div className="page-container-line-1"/>
-                    {StudentAddress.map((item) => (
+                    {StudentAddress.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -193,7 +245,7 @@ function Profile() {
                 <div className="page-base-container profilepg-container">
                     <h1>Parent/Guardian Information</h1>
                     <div className="page-container-line-1"/>
-                    {Parent1Info.map((item) => (
+                    {Parent1Info.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -202,7 +254,7 @@ function Profile() {
                     ))}
     
                     <div className="page-container-line-2"/>
-                    {Parent2Info.map((item) => (
+                    {Parent2Info.map((item: UserDataType) => (
                         <div className="profilepg-sub-container">
                             <h1>{item.Key}</h1>
                             <h2>{item.value}</h2>
@@ -228,19 +280,19 @@ function Profile() {
         return (
             <>
                 {/* Content panel 2 */}
-                {UserDocuments.map((item) => (
+                {UserDocuments.map((item: UserDataType) => (
                     <div className="page-base-container profilepg-container">
                         <h1>{item.Key}</h1>
                         <div className="page-container-line-1"/>
 
                         {openDocuments[item.Key] && (
                             <>
-                              <img src={item.img} alt={item.Key} />
+                              <img src={item.value} alt={item.Key} />
                               <div className="page-container-line-1"/>
                             </>
                         )}
                         
-                        <a href={item.img} target="_blank" rel="noopener noreferrer" className="material-icons">open_in_new</a>
+                        <a href={item.value} target="_blank" rel="noopener noreferrer" className="material-icons">open_in_new</a>
                         <span onClick={() => toggleDocument(item.Key)} >
                             { openDocuments[item.Key]
                             ? "Hide"
