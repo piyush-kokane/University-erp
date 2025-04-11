fd184e344493749c1c6a2942a026906234444448
Piyush Kokane <pjkokane21@gmail.com>
2025-04-11 20:12:11 +0530

    data fetching error handeling

--- backend/models/UserData.js
+++ backend/models/UserData.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
-const UserDataSchema = new mongoose.Schema({
-  Username: { type: String, required: true, unique: true },
+const userDataSchema = new mongoose.Schema({
   FullName: { type: String, required: true },
   FirstName: { type: String, required: true },
   LastName: { type: String, required: true },
-  contact: { type: String },
-  gmail: { type: String },
-  Prn: { type: String },
-  Branch: { type: String },
-  Term: { type: String },
-  Profile: { type: String }, // URL to the profile image
-  Banner: { type: String },  // URL to the banner image
-  Biotag: { type: String },
-  LongBio: { type: String },
-  ShortBio: { type: String }
-}, { timestamps: true });
+  contact: { type: String, required: true },
+  gmail: { type: String, required: true },
+  Prn: { type: String, required: true },
+  Branch: { type: String, required: true },
+  Term: { type: String, required: true },
+  Profile: { type: String, required: true },
+  Banner: { type: String, required: true },
+  Biotag: { type: String, required: true },
+  LongBio: { type: String, required: true },
+  ShortBio: { type: String, required: true }
+});
 
-module.exports = mongoose.model('userdatas', UserDataSchema); // userdatas is name of collectioon
+const userSchema = new mongoose.Schema({
+  Username: { type: String, required: true, unique: true },
+  UserData: { type: userDataSchema, required: true }
+});
+
+module.exports = mongoose.model('users', userSchema); // users is name of collectioon
